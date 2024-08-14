894d9f7790ad811b38a8af2068d2bf349cc05052
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-14 18:36:21 +0300

    Update NotificationController.java

--- notification-service/src/main/java/org/work/notificationservice/controller/NotificationController.java
+++ notification-service/src/main/java/org/work/notificationservice/controller/NotificationController.java
@@ -1,4 +1,36 @@
 package org.work.notificationservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.work.notificationservice.dto.NotificationDto;
+import org.work.notificationservice.service.NotificationService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notifications")
 public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @GetMapping
+    public List<NotificationDto> getAllNotifications() {
+        return notificationService.getAllNotifications();
+    }
+
+    @GetMapping("/{id}")
+    public NotificationDto getNotificationById(@PathVariable Long id) {
+        return notificationService.getNotificationById(id);
+    }
+
+    @PostMapping
+    public NotificationDto createNotification(@RequestBody NotificationDto notificationDto) {
+        return notificationService.createNotification(notificationDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNotification(@PathVariable Long id) {
+        notificationService.deleteNotification(id);
+    }
 }
